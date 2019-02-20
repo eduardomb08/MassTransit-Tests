c43771c6578c726734024d120df9522c13917a56
Eduardo Monteiro de Barros <eduardo.mb@amwins.com>
2019-02-20 15:14:32 -0500

    Added AzureServiceBus issue repro project

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using MassTransit;
+using MassTransit.AzureServiceBusTransport;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace ConsoleApp3
@@ -10,6 +12,57 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            MainAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static async Task MainAsync()
+        {
+            var bus = Bus.Factory.CreateUsingAzureServiceBus(cfg =>
+            {
+                var sbName = ConfigurationManager.AppSettings["AzureSbNamespace"];
+                var sbKeyName = ConfigurationManager.AppSettings["AzureSbKeyName"];
+                var sbSharedAccessKey = ConfigurationManager.AppSettings["AzureSbSharedAccessKey"];
+
+                var serviceUri = new Uri($"sb://{sbName}.servicebus.windows.net/");
+
+                IServiceBusHost host = cfg.Host(serviceUri, h =>
+                {
+                    h.TransportType = TransportType.NetMessaging;
+                    h.OperationTimeout = TimeSpan.FromSeconds(10);
+                    h.TokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(sbKeyName, sbSharedAccessKey, TimeSpan.FromDays(1), TokenScope.Namespace);
+                });
+
+
+                cfg.ReceiveEndpoint(host, "testqueue", ec =>
+                {
+                    ec.Consumer<MyMessageConsumer>();
+                });
+            });
+
+            bus.Start();
+
+            var i = 10;
+            while (i > 0)
+            {
+                await bus.Publish(new MyMessage() { Text = "Hello World!" });
+                i--;
+            }
         }
     }
+
+    public class MyMessageConsumer : IConsumer<MyMessage>
+    {
+        public Task Consume(ConsumeContext<MyMessage> context)
+        {
+            return Console.Out.WriteLineAsync(context.Message.Text);
+        }
+    }
+
+    public class MyMessage
+    {
+        public string Text { get; set; }
+    }
+
 }
